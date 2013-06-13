80182861b92a7fa74277ec8e1eb881669fdf7527
Steve <evenstevens280@gmail.com>
2013-06-13 15:30:54 +0200

    Fixed bug where nested statements wouldn't be parsed properly

--- sublime-css-auto-comments.py
+++ sublime-css-auto-comments.py
@@ -9,7 +9,7 @@ class CssautocommentsCommand(sublime_plugin.TextCommand):
 
 		if(cssProp != None):
 			# enter edit mode
-			self.editMode(edit, cssProp)
+			self.writeOut(edit, cssProp)
 
 	def getCursor(self):
 		cursorReg = self.view.sel()[0]
@@ -24,6 +24,7 @@ class CssautocommentsCommand(sublime_plugin.TextCommand):
 
 			cssName = cssProp = cssProp[:-1].strip()
 			cssType = ''
+			cssAttrs = ''
 
 			if (cssProp[0] == '.'):
 				cssType = 'class'
@@ -32,18 +33,27 @@ class CssautocommentsCommand(sublime_plugin.TextCommand):
 				cssType = 'id'
 				cssName = cssProp[1:]
 			else:
-				cssType = 'element'	
+				cssType = 'element'
+
+			attrs = re.match('(.*)(\[(.*)=(.*)\])', cssName)
+			if attrs:
+				cssAttrs = attrs.group(2)[1:-1]
+				cssName = attrs.group(1)
 
 			relatedCss = self.findRelatedCss(cssProp)
 			relatedSass = self.findNestedCss(cssProp)
 
-			# Combine all lists and remove dupes
-			relatedCss = relatedCss + list(set(relatedSass) - set(relatedCss))
+			# Combine all lists
+			relatedCss = relatedCss + relatedSass
+			# Remove dupes whilst preserving order
+			relatedCss = [x for i,x in enumerate(relatedCss) if i==relatedCss.index(x)]
+			#Remove empty elements
 			relatedCss = filter(None, relatedCss)
 
 			return {
 				'name' : cssName,
 				'type' : cssType,
+				'attrs': cssAttrs,
 				'related' : relatedCss
 			}
 
@@ -55,15 +65,12 @@ class CssautocommentsCommand(sublime_plugin.TextCommand):
 		for i in range(0,len(relatedCss)):
 			relatedCss[i] = self.view.substr(relatedCss[i])[len(cssProp):-1].strip()
 
-		print relatedCss[i]
-
 		return relatedCss
 
 	def findNestedCss(self, cssProp):
 		relatedCss = self.view.find_all(cssProp + "(\.|\:)?.*(\n*\s*)\{")
 
 		nestedCss = []
-		
 		
 		recurseLimit = 100
 
@@ -75,32 +82,37 @@ class CssautocommentsCommand(sublime_plugin.TextCommand):
 			offset = 0
 			bracketStack = []
 			recurseAmount = 0
+			firstBracketFound = False
 
 			point = relatedCss[i].begin()
 
 			line = self.getLineContents(self.view.line(point).begin())
 
-		# Start with somethign on the bracket stack
+			# Start with something on the bracket stack
 			bracketStack.append('{')
 
-		# Start building the CSS block to parse
+			# Start building the CSS block to parse
 			cssBlock += line.replace(" ","")
 
-		#This is for keeping track of what line its on
+			#This is for keeping track of what line its on
 			linelen = len(line)
 			offset = offset + linelen + 1
 
 			# Keep going when the brackets are unmatched
 			while len(bracketStack) > 0 and recurseAmount < recurseLimit:		
 
-			# Stack operations
-				if recurseAmount == 0:
-					negateBrackets = -1
-				else:
-					negateBrackets = 0
+				print bracketStack
+				print recurseAmount
 
-				for j in range (0,line.count('{') + negateBrackets):
-					bracketStack.append('{')
+				# Stack operations
+				for j in range (0,line.count('{')):
+					if firstBracketFound:
+						bracketStack.append('{')
+
+					if(line.count('{')):
+						firstBracketFound = True					
+
+
 				for j in range (0,line.count('}')):	
 					bracketStack.pop()
 
@@ -116,39 +128,42 @@ class CssautocommentsCommand(sublime_plugin.TextCommand):
 				if recurseAmount == 99:
 					print "Recursion limit hit. Check your brackets are evenly matched for element " + self.view.substr(relatedCss[i])[:-1]
 
+			bracketCount = 0
+			fullClass = ""
+			hookFound = True
+			hookedClass = True
+			# Go through the concatted css block to find stuff
+			# Could probably do this in the while loop up there ^^^
+			# Pretty funny logic. Don't try and follow it D:
+			for j in range (0, len(cssBlock)):
+				if cssBlock[j] == "{":
+					bracketCount+=1
 
-		bracketCount = 0
-		fullClass = ""
-		hookFound = True
-		hookedClass = True
-		# Go through the concatted css block to find stuff
-		# Could probably do this in the while loop up there ^^^
-		for j in range (0, len(cssBlock)):
-			print bracketCount
-			if cssBlock[j] == "{":
-				bracketCount+=1
-
-				if hookFound == False:
-					hookedClass = False
-
-				hookFound = False
-
-			if cssBlock[j] == "}":
-				bracketCount-=1
-
-			if cssBlock[j] == "&":
-				hookFound =  True
-				if hookedClass:
-					tempString = cssBlock[j+1:]
-					if bracketCount == 1:
-						fullClass = tempString.partition('{')[0]
+					if hookFound == True:
+						hookedClass = True
 					else:
-						print tempString
-						fullClass = fullClass + tempString.partition('{')[0]
+						hookedClass = False
 
-					hookedClass = False
+					hookFound = False
 
-					nestedCss.append(fullClass)
+				if cssBlock[j] == "}":
+					bracketCount-=1
+
+					if bracketCount == 1:
+						hookedClass = True
+
+				if cssBlock[j] == "&":
+					if hookedClass:
+						hookFound =  True
+						tempString = cssBlock[j+1:]
+						if bracketCount == 1:
+							fullClass = tempString.partition('{')[0]
+						else:
+							fullClass = fullClass + tempString.partition('{')[0]
+
+						hookedClass = False
+
+						nestedCss.append(fullClass)
 
 		return nestedCss
 
@@ -156,20 +171,24 @@ class CssautocommentsCommand(sublime_plugin.TextCommand):
 		return self.view.substr(self.view.line(line))
 
 
-	def editMode(self, edit, cssAttr):
+	def writeOut(self, edit, cssAttr):
 
 		markup = ''
 
 		cssName = cssAttr['name']
 		cssType = cssAttr['type']
+		cssAttrs = cssAttr['attrs']
 		relatedCss = cssAttr['related']
 
+		if cssAttrs:
+			cssAttrs = ' ' + cssAttrs
+
 		if(cssType == 'class'):
-			markup = '<div class="' + cssName + '">markup</div>'
+			markup = '<div class="' + cssName + cssAttrs + '">markup</div>'
 		elif(cssType == 'id'):
-			markup = '<div id="' + cssName + '">markup</div>'
+			markup = '<div id="' + cssName + cssAttrs + '">markup</div>'
 		elif(cssType == 'element'):
-			markup = '<' + cssName + '>Markup</' + cssName + '>'
+			markup = '<' + cssName + cssAttrs + '>Markup</' + cssName + '>'
 
 
 		# replace the current cursor with a comment block and auto fill in all the attributes
